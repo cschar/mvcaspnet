9daaf376d928f2b912d4f990110c5d53025d60f1
unknown <codyscharfe@gmail.com>
2017-12-27 20:46:59 -0500

    'init'

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public string Welcome2(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
